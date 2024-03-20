c0759ff9efa92aff25d9ccdeb0505924d553e79a
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2024-03-20 06:55:14 +0500

    darslik

---
+++ JS da operatorlar/main.js
@@ -0,0 +1,68 @@
+/**
+ * Javascriptda operatorlar.
+ * Dasturlash tillarida bir nechta operatorlar mavjud; yani bu operatorlar dasturlashta qo'llaniladi.
+ * Bular :
+ * 1. Arifmetik operatorlar
+ * 2. Tayinlash operatorlar
+ * 3. String operatorlar
+ * 4. Solishtirish operatorlar
+ * 5. Mantiqiy operatorlar
+ * 6. Bitli operatorlar
+ * 
+ * 
+ * 
+ * 
+ * Operatorlar dastur davomida natijani hosil qilish uchun kerakli barcha ishni qiladi.
+ * Arifmetik operatorlar:
+ *   "+" -  qo'shish operatori
+ *   "-" -  ayirishi operatori
+ *   "*" - ko'paytirish operatori
+ *   "/" - bo'lish operatori
+ *   "**" - daraja operatori (ya'ni sonni darajasini topib beruvchi operator)
+ *   "%"  - modul operatori (ya'ni qoldiqli bo'lish)
+ *   "++" - bir qo'shib borish 
+ *   "--" - bir kamaytirib borish
+ */
+
+
+//  Javascriptda tayinlash operatori
+/**
+ *     "=" -            x=y             x=y             x o'zgaruvchiga y qiymatni berish
+ *      "+=" -          x+=y            x=x+y           x o'zgaruvchiga x ning avvalgi qiymatiga y ning qiymatni qo'shib qo'yish va yangi qiymatni saqlash
+ *      "-=" -          x-=y            x=x-y           x o'zgaruvchiga x ning avvalgi qiymatidan y ning qiymatini ayirib uni yangi qiymat qilib saqlash
+ *      "*" -           x*=y            x=x*y           x o'zgaruvchiga x ning avvalgi qiymatini y ning qiymatiga ko'paytirib uni yangi qiymat qilib saqlash
+ *      "/" -           x/=y            x=x/y           x o'zgaruvchiga x ning avvalgi qiymatiga y ning qiymatiga bo'lib uni yangi qiymat qilib saqlash 
+ *      "%" -           x%=y            x=x%y           x o'zgaruvchiga x ning avvalgi qiymatini y ga qoldiqli bo'lib natijada chiqadigan qoldiqini yangi qiymat sifatida saqlash
+ *      "**" -          x**=y           x=x**y          x o'zgaruvchiga x ning avvalgi qiymatini y ning darajasini topib uni yangi qiymat qilib saqlash
+ */
+
+// Javascriptda String operatorlar. bu yerda matnlarni hozircha qo'shish amalidan foydalanib turamiz;
+/**
+ * var ism="Doston";
+ * var familiya = "Urunboyev";
+ * var otasiningismi = "Ortiqboy o'g'lir"
+ * var fio = ism + " " + familiya + " " + otasiningismi;
+ */
+
+
+// Javascripda solishtirish operatorlari
+
+/**
+ *      "==" -          O'zaro teng
+ *      "===" -         Qat'iy teng
+ *      "!=" -          Teng emas
+ *      "!==" -         Qat'iy teng emas
+ *      ">" -           Katta
+ *      "<" -           Kichik
+ *      ">=" -          Katta yoki teng
+ *      "<=" -          Kichik yoki teng      
+ */
+
+
+//  Javascriptda mantiqiy operatorlar
+
+/**
+ *          "&&" -      mantiqiy ko'paytirish
+ *          "||" -      mantiqiy qo'shish
+ *          "!" -       mantiqiy inkor  
+ */
